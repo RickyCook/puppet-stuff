54a6c80dfafc8d9c450e8686c28196b90f942365
Ricky Cook <ricky@infoxchange.net.au>
2013-05-20 21:09:58 +1000

    Fixed exec in sublime manifest

--- modules/panda_sublimetext/manifests/init.pp
+++ modules/panda_sublimetext/manifests/init.pp
@@ -3,8 +3,8 @@ class panda_sublimetext {
 		/x86_64|amd64/ => 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1%20x64.tar.bz2',
 		/x86|i386/ => 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1.tar.bz2',
 	}
-	Exec { 'install_puppet':
-		command => 'wget -O /tmp/sublime.tar.bz2 $url && tar --overwrite -xjf /tmp/sublime.tar.bz2 -C /opt',
+	exec { 'install_puppet':
+		command => "wget -O /tmp/sublime.tar.bz2 $url && tar --overwrite -xjf /tmp/sublime.tar.bz2 -C /opt",
 		unless =>'test -f "/opt/Sublime Text 2/sublime_text" && [ `/opt/Sublime\ Text\ 2/sublime_text --version|grep --only-matching "[0-9]*$"` -ge 2217 ]',
 	}
 }
